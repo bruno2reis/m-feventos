1161aae910ad4b647365c29e3ecbd346fa1cd763
bruno <brunodosreis21@gmail.com>
2018-11-05 15:09:03 -0200

    new site completo

---
+++ MF_Eventos/resources/views/pages/index.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Pizza - Free Bootstrap 4 Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nothing+You+Could+Do" rel="stylesheet">
+
+    <link href="{{  URL::asset('css/animate.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/open-iconic-bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/owl.carousel.min.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/owl.theme.default.min.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/magnific-popup.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/aos.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/ionicons.min.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/bootstrap-datepicker.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/jquery.timepicker.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/jquery.timepicker.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/flaticon.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/icomoon.css') }}" rel="stylesheet">
+    <link href="{{  URL::asset('css/style.css') }}" rel="stylesheet">
+
+</head>
+
+<body>
+@include('layout.principal')
+</body>
+
+
+<!-- loader -->
+<div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
+
+
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ URL::asset('/js/jquery-migrate-3.0.1.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/popper.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/bootstrap.min.js') }}"></script>
+
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.easing.1.3.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.waypoints.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.stellar.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/owl.carousel.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.magnific-popup.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/aos.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.animateNumber.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/bootstrap-datepicker.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/jquery.timepicker.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/scrollax.min.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/google-map.js') }}"></script>
+<script type="text/javascript" src="{{ URL::asset('/js/main.js') }}"></script>
+</html>
---
+++ MF_Eventos/resources/views/view.blade.php
@@ -0,0 +1,7 @@
+@push('styles')
+    <link href="{{ asset('sass/animate.css') }}" rel="stylesheet">
+@endpush
+
+@push('scripts')
+    <script type="text/javascript" src="{{ asset('js/scripts.js') }}"></script>
+@endpush
